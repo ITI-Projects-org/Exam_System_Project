b799818c5dad7898ef274a63f2e86db1b2dde355
melsa <melsa@MUHMD-SABAGH-PC>
2025-07-08 23:00:23 +0300

    add data seeder again to backend and commented, and add home component to frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer],
+  imports: [Footer, Home, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
