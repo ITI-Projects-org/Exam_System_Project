8578b379755b76357db8735af10cf8e72e96b9ff
Ahmed Khaled <ahmedkhaled0048@gmail.com>
2025-07-09 22:43:31 +0300

    merging

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,6 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
-} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
