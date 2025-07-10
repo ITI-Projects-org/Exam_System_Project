a286a6bc0c65328b32d585ba39f840877b8745d1
melsa <melsa@MUHMD-SABAGH-PC>
2025-07-10 12:27:44 +0300

    Navbar and Footer added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
-import { Home } from './pages/home/home';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Footer, Home, Navbar, RouterOutlet],
+  imports: [Footer, Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
