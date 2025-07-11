40c9893b8ae3e6fb48b16aa91192e190fc9f55e8
moomenabdelshaheed <moomentheone@gmail.com>
2025-07-12 01:16:23 +0300

    test

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
 import { TeachersList } from './pages/teacher/teachers-list/teachers-list';
 import { AuthService } from './services/auth-service';
+import { Courses } from "./pages/Courses/courses";
 
 @Component({
   selector: 'app-root',
-  imports: [Footer, Navbar, RouterOutlet],
+  imports: [Footer, Navbar, RouterOutlet, Courses],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
